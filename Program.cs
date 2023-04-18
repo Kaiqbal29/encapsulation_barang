83f7b748dc58f3cabe5fe7e9e924b5b5edbbaa28
Kaiqbal29 <iqbalfaris@students.amikom.ac.id>
2023-04-18 23:20:48 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using encapsulation;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Barang brg1 = new Barang("Cocacola", 10000);
+        Barang brg2 = new Barang("Chitato", 9000);
+        Barang brg3 = new Barang("EsKrim", 19000);
+
+        Console.WriteLine("Total Barang : " + Barang.TotalBarang);
+        Console.WriteLine("===============================================");
+
+        Console.WriteLine("Name  : " + brg1.Name);
+        Console.WriteLine("Price : Rp." + brg1.Price);
+        Console.WriteLine();
+        Console.WriteLine("Name  : " + brg2.Name);
+        Console.WriteLine("Price : Rp." + brg2.Price);
+        Console.WriteLine();
+        Console.WriteLine("Name  : " + brg3.Name);
+        Console.WriteLine("Price : Rp." + brg3.Price);
+
+
+        Console.ReadKey();
+    }
+}
---
+++ barang.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace encapsulation
+{
+    class Barang
+    {
+        public static int TotalBarang = 0;
+        private string name;
+        private int price;
+
+        public Barang(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+            TotalBarang++;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
